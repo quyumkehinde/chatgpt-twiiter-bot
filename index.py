1dfaa285f925731341520972ad08ccd9b67bb834
Quyum Kehinde <quyumkehinde@gmail.com>
2022-12-10 02:50:23 +0100

    refactor: clean up implementation

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 import tweepy
 import openai
 import os
+import traceback
 
-# Authenticate to Twitter
 CONSUMER_KEY = os.getenv("CONSUMER_KEY")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
@@ -10,24 +10,23 @@ ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+try:
+    auth = tweepy.OAuthHandler(
+        consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
 
-openai.api_key = OPENAI_API_KEY
+    openai.api_key = OPENAI_API_KEY
 
-api = tweepy.API(auth)
+    api = tweepy.API(auth)
 
+    def handle_tweet(tweet):
+        response = openai.Completion.create(
+            model="text-davinci-003", prompt=tweet.text)
 
-def handle_tweet(tweet):
-    response = openai.Completion.create(
-        model="text-davinci-003", prompt=tweet.text)
+        api.update_status(response["choices"][0].text, tweet.id)
 
-    api.update_status(response, tweet.id)
-
-
-stream = tweepy.StreamingClient("Bearer Token")
-stream.on_tweet = handle_tweet
-stream.filter(track=["@ChatGPTBot_"])
-
-
-# TODO: add logging implementation
+    stream = tweepy.StreamingClient("Bearer Token")
+    stream.add_rules(tweepy.StreamRule("@ChatGPTBot_"))
+    stream.on_tweet = handle_tweet
+    stream.filter()
+except Exception:
+    traceback.print_exception()
